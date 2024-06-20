452cb1389128b5fee3f26c683ec260b96ff92e06
jason <784442670@qq.com>
2024-06-20 16:41:02 +0800

    新增支付宝接口工具API

---
+++ HmExtension.Pay/AlipayContext.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Aop.Api;
+using Aop.Api.Request;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace HmExtension.Pay;
+
+/// <summary>
+/// 支付宝全局上下文
+/// </summary>
+public class AlipayContext
+{
+    // IAopClient client = new
+    // DefaultAopClient(
+    // "https://openapi.alipay.com/gateway.do",
+    // APPID, APP_PRIVATE_KEY,
+    // "json", "1.0",
+    // "RSA2",
+    // ALIPAY_PUBLIC_KEY,
+    // CHARSET,
+    // false);
+    private static IAopClient _client;
+
+    /// <summary>
+    /// 初始化支付宝全局上下文
+    /// </summary>
+    /// <param name="serverUrl">网关地址</param>
+    /// <param name="appId">AppID</param>
+    /// <param name="privateKey">应用私钥</param>
+    /// <param name="alipayPublicKey">支付宝公钥</param>
+    /// <param name="format">数据格式,默认: json</param>
+    /// <param name="signType">签名类型,默认: RSA2</param>
+    /// <param name="charset">字符集编码</param>
+    /// <param name="keyFromFile">密钥是否来自文件,如果为true,则privateKey和alipayPublicKey参数传递密钥文件路径</param>
+    public static void Init(
+        string serverUrl,
+        string appId,
+        string privateKey,
+        string alipayPublicKey,
+        string format = "json",
+        string signType = "RSA2",
+        string charset = "utf-8",
+        bool keyFromFile = false)
+    {
+        AlipayConfig config = new AlipayConfig
+        {
+            ServerUrl = serverUrl,
+            AppId = appId,
+            PrivateKey = privateKey,
+            Format = format,
+            SignType = signType,
+            AlipayPublicKey = alipayPublicKey,
+            Charset = charset
+        };
+        if (keyFromFile)
+        {
+            config.PrivateKey = string.IsNullOrWhiteSpace(privateKey) ? "" : System.IO.File.ReadAllText(privateKey);
+            config.AlipayPublicKey = string.IsNullOrWhiteSpace(alipayPublicKey)
+                ? ""
+                : System.IO.File.ReadAllText(alipayPublicKey);
+        }
+
+        Init(config);
+    }
+
+    /// <summary>
+    /// 初始化支付宝全局上下文
+    /// </summary>
+    /// <param name="config">支付宝配置对象</param>
+    public static void Init(AlipayConfig config)
+    {
+        _client = new DefaultAopClient(config);
+    }
+
+    /// <summary>
+    /// 执行支付宝请求
+    /// </summary>
+    /// <typeparam name="T">请求类型</typeparam>
+    /// <param name="request">请求对象</param>
+    /// <returns>响应对象</returns>
+    /// <exception cref="Exception"></exception>
+    public static T Execute<T>(IAopRequest<T> request) where T : AopResponse
+    {
+        if (_client == null)
+        {
+            throw new Exception("请先调用[HmExtension.Pay.AlipayContext.Init]方法,初始化支付宝全局上下文");
+        }
+
+        return _client.Execute(request);
+    }
+
+
+    public static void Execption<T>(T response) where T : AopResponse
+    {
+        if (response.IsError)
+        {
+            throw new Exception($"调用失败[{response.Code}-{response.SubCode}] {response.Msg}:{response.SubMsg}");
+        }
+        
+    }
+}
---
+++ HmExtension.Pay/AopResponseExtension.cs
@@ -0,0 +1,19 @@
+﻿using Aop.Api;
+
+namespace HmExtension.Pay;
+/// <summary>
+/// 支付宝响应扩展
+/// </summary>
+public static class AopResponseExtension
+{
+    /// <summary>
+    /// 是否成功
+    /// </summary>
+    /// <param name="response"></param>
+    /// <returns></returns>
+    public static bool IsSuccess(this AopResponse response)
+    {
+        return !response.IsError;
+    }
+    
+}
