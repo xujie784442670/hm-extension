ab03fe50e6dffa047172f83a3cb2f4bb5cc7a3b4
jason <784442670@qq.com>
2024-07-29 10:44:45 +0800

    分离Js引擎部分代码

---
+++ HmExtension.Commons/Model/Color.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace HmExtension.Commons.Model;
+
+public class Color
+{
+    /// <summary>
+    /// 红色(Red)
+    /// </summary>
+    public byte R { get; set; }
+    /// <summary>
+    /// 绿色(Green)
+    /// </summary>
+    public byte G { get; set; }
+    /// <summary>
+    /// 蓝色(Blue)
+    /// </summary>
+    public byte B { get; set; }
+    /// <summary>
+    /// 透明度(Alpha)
+    /// </summary>
+    public byte A { get; set; }
+
+    public Color(byte r, byte g, byte b, byte a = 255)
+    {
+        R = r;
+        G = g;
+        B = b;
+        A = a;
+    }
+
+    public Color(string hex)
+    {
+        var color = FromArgb(hex);
+        R = color.R;
+        G = color.G;
+        B = color.B;
+        A = color.A;
+    }
+
+    public static string ToHex(Color color)
+    {
+        return $"#{color.R:X2}{color.G:X2}{color.B:X2}{color.A:X2}";
+    }
+
+
+    public static Color FromArgb(byte a, byte r, byte g, byte b=255)
+    {
+        return new Color(r, g, b, a);
+    }
+
+    public static Color FromArgb(string hex)
+    {
+        var color = new Color(0,0,0);
+        if (hex.StartsWith("#"))
+        {
+            hex = hex[1..];
+        }
+
+        // RRGGBB
+        if (hex.Length == 6)
+        {
+            color.R = byte.Parse(hex[0..2], System.Globalization.NumberStyles.HexNumber);
+            color.G = byte.Parse(hex[2..4], System.Globalization.NumberStyles.HexNumber);
+            color.B = byte.Parse(hex[4..6], System.Globalization.NumberStyles.HexNumber);
+            color.A = 255;
+        }
+        // RRGGBBAA
+        else if (hex.Length == 8)
+        {
+            color.R = byte.Parse(hex[0..2], System.Globalization.NumberStyles.HexNumber);
+            color.G = byte.Parse(hex[2..4], System.Globalization.NumberStyles.HexNumber);
+            color.B = byte.Parse(hex[4..6], System.Globalization.NumberStyles.HexNumber);
+            color.A = byte.Parse(hex[6..8], System.Globalization.NumberStyles.HexNumber);
+        }
+        // RGB
+        else if (hex.Length == 3)
+        {
+            color.R = byte.Parse(hex[0] + hex[0].ToString(), System.Globalization.NumberStyles.HexNumber);
+            color.G = byte.Parse(hex[1] + hex[1].ToString(), System.Globalization.NumberStyles.HexNumber);
+            color.B = byte.Parse(hex[2] + hex[2].ToString(), System.Globalization.NumberStyles.HexNumber);
+            color.A = 255;
+        }
+        // RGBA
+        else if (hex.Length == 4)
+        {
+            color.R = byte.Parse(hex[0] + hex[0].ToString(), System.Globalization.NumberStyles.HexNumber);
+            color.G = byte.Parse(hex[1] + hex[1].ToString(), System.Globalization.NumberStyles.HexNumber);
+            color.B = byte.Parse(hex[2] + hex[2].ToString(), System.Globalization.NumberStyles.HexNumber);
+            color.A = byte.Parse(hex[3] + hex[3].ToString(), System.Globalization.NumberStyles.HexNumber);
+        }
+        else
+        {
+            throw new ArgumentException("Invalid hex string");
+        }
+        return color;
+    }
+}
---
+++ HmExtension.Commons/utils/RandomHelper.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using HmExtension.Commons.Model;
+
+namespace HmExtension.Commons.utils;
+
+/// <summary>
+/// 随机帮助类
+/// </summary>
+public class RandomHelper
+{
+    public static Random Random { get; } = new Random();
+
+    public const string LowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+
+    public const string UppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public const string Letters = LowercaseLetters + UppercaseLetters;
+
+    public const string Numbers = "0123456789";
+
+    public const string LettersAndNumbers = Letters + Numbers;
+
+    /// <summary>
+    /// 获取一个随机数,取值范围为[minValue,maxValue)
+    /// </summary>
+    /// <param name="minValue">
+    ///     最小值
+    /// </param>
+    /// <param name="maxValue">最大值</param>
+    /// <returns></returns>
+    public static int Next(int minValue, int maxValue)
+    {
+        return Random.Next(minValue, maxValue);
+    }
+
+    /// <summary>
+    /// 获取一个随机数,取值范围为[0,maxValue)
+    /// </summary>
+    /// <param name="maxValue">最大值</param>
+    /// <returns></returns>
+    public static int Next(int maxValue)
+    {
+        return Random.Next(maxValue);
+    }
+
+    /// <summary>
+    /// 获取一个随机数,取值范围为[0,int.MaxValue)
+    /// </summary>
+    /// <returns></returns>
+    public static int Next()
+    {
+        return Random.Next();
+    }
+
+    /// <summary>
+    /// 获取一个随机数,取值范围为[0,1)
+    /// </summary>
+    /// <returns></returns>
+    public static double NextDouble()
+    {
+        return Random.NextDouble();
+    }
+
+    /// <summary>
+    /// 生成随机字节数组
+    /// </summary>
+    /// <param name="buffer"></param>
+    public static void NextBytes(byte[] buffer)
+    {
+        Random.NextBytes(buffer);
+    }
+
+    /// <summary>
+    /// 生成随机字节数组
+    /// </summary>
+    /// <param name="buffer"></param>
+    public static void NextBytes(Span<byte> buffer)
+    {
+        var bytes = new byte[buffer.Length];
+        Random.NextBytes(bytes);
+        bytes.CopyTo(buffer);
+    }
+
+    /// <summary>
+    /// 获取一个随机布尔值
+    /// </summary>
+    /// <returns></returns>
+    public static bool NextBool()
+    {
+        return Random.Next(2) == 1;
+    }
+
+    /// <summary>
+    /// 获取一个随机枚举值
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static T NextEnum<T>()
+    {
+        var values = Enum.GetValues(typeof(T));
+        return (T)values.GetValue(Random.Next(values.Length));
+    }
+
+    /// <summary>
+    /// 获取一个随机枚举值
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="values"></param>
+    /// <returns></returns>
+    public static T NextEnum<T>(params T[] values)
+    {
+        return values[Random.Next(values.Length)];
+    }
+
+    /// <summary>
+    /// 获取一个随机颜色
+    /// </summary>
+    /// <param name="isAlpha">是否随机透明度</param>
+    /// <returns></returns>
+    public static Color NextColor(bool isAlpha)
+    {
+        return Color.FromArgb((byte)Next(256), (byte)Next(256), (byte)Next(256), (byte)(isAlpha ? Next(256) : 255));
+    }
+
+    /// <summary>
+    /// 随机获取一个指定长度的字符串
+    /// </summary>
+    /// <param name="length">字符串长度,如果为0,则会随机生成一个[1,100)的长度</param>
+    /// <param name="chars">字符串列表</param>
+    /// <returns></returns>
+    public static string NextString(int length=0, string chars = LettersAndNumbers)
+    {
+        if (length <= 0)
+        {
+            length = Next(1,100);
+        }
+        var buffer = new char[length];
+        for (var i = 0; i < length; i++)
+        {
+            buffer[i] = chars[Next(chars.Length)];
+        }
+
+        return new string(buffer);
+    }
+
+}
