d4c16bf40b3bb2066d9982f89b876cee4575336e
jason <784442670@qq.com>
2024-07-29 10:45:16 +0800

    通用二维码帮助类

---
+++ HmExtension.QRCode/BitmapExtension.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+using ZXing;
+
+namespace HmExtension.QRCode;
+/// <summary>
+/// 图片扩展类(v1.0.0.3)
+/// </summary>
+public static class BitmapExtension
+{
+    /// <summary>
+    /// 解析二维码
+    /// </summary>
+    /// <param name="barcodeBitmap"></param>
+    /// <returns></returns>
+    public static string DecodeQrCode(this Bitmap barcodeBitmap)
+    {
+        BarcodeReader reader = new BarcodeReader();
+        reader.Options.CharacterSet = "UTF-8";
+        Result result = reader.Decode(barcodeBitmap);
+        return result == null ? "" : result.Text;
+    }
+}
